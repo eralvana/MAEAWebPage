47e3dd73c39b49877fcfa52076856ae2119b6e02
Shino@TerminalMBA <ernestociencias@yahoo.com.mx>
2021-08-30 13:36:09 -0500

    Iconos

--- template/src/components/Footer/index.js
+++ template/src/components/Footer/index.js
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import Context from "../../context.js";
 
 import { Box } from "@material-ui/core";
+import { Icon } from '@iconify/react';
 
 import "./styles.css";
 
@@ -10,10 +11,14 @@ function Footer() {
   return (
     <Box component="footer" textAlign="center">
       <ul>
-        <li display="inline"><a href={value.info.links.github} target="_blank" rel="noreferrer">GitHub</a></li>
-        <li><a href={value.info.links.twitter} target="_blank" rel="noreferrer">Twitter</a></li>
-        <li><a href={value.info.links.stackoverflow} target="_blank" rel="noreferrer">Stackoverflow</a></li>
-        <li><a href={value.info.links.linkedin} target="_blank" rel="noreferrer">LinkedIn</a></li>
+        <li display="inline"><a href={value.info.links.github} target="_blank" rel="noreferrer"><Icon icon="jam:github" width="32" height="32"/>
+</a></li>
+        <li><a href={value.info.links.twitter} target="_blank" rel="noreferrer"><Icon icon="jam:twitter" width="32" height="32" />
+</a></li>
+        <li><a href={value.info.links.stackoverflow} target="_blank" rel="noreferrer"><Icon icon="jam:stackoverflow" width="32" height="32" />
+</a></li>
+        <li><a href={value.info.links.linkedin} target="_blank" rel="noreferrer"><Icon icon="jam:linkedin" width="32" height="32" />
+</a></li>
       </ul>
       <div>{value.info.name} &copy; 2021</div>
     </Box>
